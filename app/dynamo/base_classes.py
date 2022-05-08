4c814a7e1bd9e110e5ea4a5aa1aefa3bbc325cfa
Sydney Andrade <syddevious@gmail.com>
2022-05-08 23:20:36 +0000

    working dynamo db

--- app/dynamo/base_classes.py
+++ app/dynamo/base_classes.py
@@ -1,4 +1,7 @@
 from app.settings import DYNAMO_HOST, DYNAMO_PORT
 
+
 class BaseMeta:
-    host = f"{DYNAMO_HOST}:{DYNAMO_PORT}"
+    host = f"http://{DYNAMO_HOST}:{DYNAMO_PORT}"
+    write_capacity_units = 10
+    read_capacity_units = 250
--- app/dynamo/test_model.py
+++ app/dynamo/test_model.py
@@ -1,6 +1,19 @@
-from pynamodb.models import Model
-from pynamodb.attributes import UnicodeAttribute
+"""
+TODO: Delete this file!!!
+"""
+
 from app.dynamo.base_classes import BaseMeta
+from pynamodb.attributes import NumberAttribute, UnicodeAttribute
+from pynamodb.models import Model
+
 
 class TestModel(Model):
-    class Meta(BaseMeta)
+    class Meta(BaseMeta):
+        table_name = "TestTable"
+
+    field_1 = UnicodeAttribute(hash_key=True)
+    field_2 = NumberAttribute()
+
+
+if not TestModel.exists():
+    TestModel.create_table(wait=True)
