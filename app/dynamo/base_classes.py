70ec586ce1ca0af95f6f7562e451f772127fd22e
Sydney Andrade <syddevious@gmail.com>
2022-05-10 23:41:08 +0000

    some cleanup

--- app/dynamo/base_classes.py
+++ app/dynamo/base_classes.py
@@ -2,6 +2,11 @@ from app.settings import DYNAMO_HOST, DYNAMO_PORT
 
 
 class BaseMeta:
+    """
+    Base meta class for dynamo models.
+    to avoid having to specify common meta data.
+    """
+
     host = f"http://{DYNAMO_HOST}:{DYNAMO_PORT}"
     write_capacity_units = 10
     read_capacity_units = 250
