f4dec0d7f4b4ac3ee16d6930b88b51287dc240da
SydDevious <asydney25@ymail.com>
2022-05-29 21:56:23 +0000

    permissions query done? need testing

--- app/model_operators/permissions.py
+++ app/model_operators/permissions.py
@@ -1,29 +1,24 @@
 from typing import List
 
-from app.models.permissions import (Permission, Role, RolePermission,
-                                    UserPermission, UserRole)
+from app.models.permissions import Permission, Role, RolePermission, UserPermission, UserRole
 from app.models.user import User, UserSession
 from app.postgres_db import DatabaseSession
 from app.schemas.user import UserCreate, UserCreateBase
 from app.settings import HASH_CONTEXT
-from sqlalchemy import and_, func, null, select
+from sqlalchemy import and_, case, func, null, select
 
 
 class PermissionsOperator:
     @staticmethod
     def get_query_for_user_permissions_by_id(
         db: DatabaseSession,
-        user_ids: List[int] = None,
+        user_id: int,
     ):
 
         # role permissions
         role_permissions = (
             select(
-                User.id.label("user_id"),
-                Role.id.label("role_id"),
-                Role.name.label("role_name"),
                 Permission.id.label("permission_id"),
-                Permission.name.label("permission_name"),
                 (True).label("permission_allowed"),
             )
             .select_from(User)
@@ -31,38 +26,43 @@ class PermissionsOperator:
             .outerjoin(Role, Role.id == UserRole.role_id)
             .outerjoin(RolePermission, RolePermission.role_id == Role.id)
             .outerjoin(Permission, Permission.id == RolePermission.permission_id)
-            .where(User.id.in_(user_ids))
-        ).subquery()
+            .where(User.id == user_id)
+        )
 
         # direct permissions
         direct_permissions = (
             select(
-                User.id.label("user_id"),
-                null().label("role_id"),
-                null().label("role_name"),
                 Permission.id.label("permission_id"),
-                Permission.name.label("permission_name"),
                 UserPermission.allowed.label("permission_allowed"),
             )
             .select_from(User)
             .outerjoin(UserPermission, UserPermission.user_id == User.id)
             .outerjoin(Permission, Permission.id == UserPermission.permission_id)
-            .where(User.id.in_(user_ids))
-        ).subquery()
+            .where(User.id == user_id)
+        )
 
+        union_sq = role_permissions.union(direct_permissions).subquery()
 
-        (
-            select()
-            # TODO: write query that joins both role permissions and direct permissions. 
-            """
-            - role permissions allow a user with a role to have all the permissions associated with that role. 
-                - if you want to assign a set of permissions to a user, you can use a role to do so.
-            - if a user has a role that allows a permission, but then has that specific permission set to not allowed directly, 
-                - the user permission takes final say. So you can assign a role of permissions, but then disallow one or two permissions within that role,
-                on a specific user.
-
-
-                SEE TRELLO FOR DETAILS
-            
-            """
+        return (
+            select(
+                Permission.id,
+                Permission.name,
+                case(
+                    [
+                        (
+                            func.count(union_sq.permission_allowed).filter(
+                                and_(
+                                    union_sq.c.permission_allowed.is_(False),
+                                    union_sq.c.permission_id == Permission.id,
+                                )
+                            )
+                            > 0,
+                            False,
+                        )
+                    ],
+                    else_=union_sq.c.permission_allowed,
+                ).label("allowed"),
+            )
+            .select_from(Permission)
+            .join(union_sq, union_sq.c.permission_id == Permission.id)
         )
